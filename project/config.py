1ec755b2d8d04c654de8d0008ab2dee7851428c8
Maksim Grafskiy <m.grafskiy@mts.ru>
2022-11-04 20:00:22 +0300

    upd

--- project/config.py
+++ project/config.py
@@ -39,12 +39,12 @@ class BaseConfig:
     CELERY_TASK_DEFAULT_QUEUE: str = "default"
     # Force all queues to be explicitly listed in `CELERY_TASK_QUEUES` to help prevent typos
     CELERY_TASK_CREATE_MISSING_QUEUES: bool = False
-    CELERY_TASK_QUEUES: list = (
+    CELERY_TASK_QUEUES: list = [
         # need to define default queue here or exception would be raised
         Queue("default"),
         Queue("high_priority"),
         Queue("low_priority"),
-    )
+    ]
     # - Celery multiple queues example
 
     # + Best practics
